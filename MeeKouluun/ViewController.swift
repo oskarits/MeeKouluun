b8e0d39be59dbb8bf6cbd7b7c57a30691715f126
Oskari Sieranen <oskari.sieranen@gmail.com>
2019-11-12 14:31:28 +0200

    Base xcodeproject, created CI

---
+++ MeeKouluun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeeKouluun
+//
+//  Created by Oskari Sieranen on 12.11.2019.
+//  Copyright © 2019 Empathy Slayers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
