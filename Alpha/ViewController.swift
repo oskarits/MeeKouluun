a13c6afd549b1539252e4a79ba73f38fc029edc8
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-19 10:03:09 +0200

    Initial Commit

---
+++ Alpha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alpha
+//
+//  Created by iosdev on 19/11/2019.
+//  Copyright © 2019 Jari Pietikäinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AlphaTests/AlphaTests.swift
@@ -0,0 +1,34 @@
+//
+//  AlphaTests.swift
+//  AlphaTests
+//
+//  Created by iosdev on 19/11/2019.
+//  Copyright © 2019 Jari Pietikäinen. All rights reserved.
+//
+
+import XCTest
+@testable import Alpha
+
+class AlphaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
