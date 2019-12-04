84653a11c78659b1caadf45a2f9742b667becdc2
smolmeri <tuomas.paavolainen@metropolia.fi>
2019-12-04 11:45:06 +0200

    cleaning up questionstory

--- Alpha/QuestionsStory/PageViewControllerTest.swift
+++ Alpha/QuestionsStory/PageViewControllerTest.swift
@@ -1,90 +1,90 @@
-
-import UIKit
-
-class PageViewControllerTest: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    
-    lazy var viewControllerList: [UIViewController] = {
-        let sb = UIStoryboard(name: "Questions", bundle: nil)
-        let vc1 = sb.instantiateViewController(withIdentifier: "Questions1")
-        let vc2 = sb.instantiateViewController(withIdentifier: "Questions2")
-        //let vc3 = sb.instantiateViewController(withIdentifier: "Questions2")
-        
-        return [vc1, vc2]
-    }()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.dataSource = self
-        self.delegate = self
-        if let firstViewController = viewControllerList.first {
-            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
-        }
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        for view in self.view.subviews {
-            if view is UIScrollView {
-                view.frame = UIScreen.main.bounds
-            } else if view is UIPageControl {
-                view.backgroundColor = UIColor.clear
-            }
-        }
-    }
-    
-    
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
-        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
-            return nil
-        }
-        
-        let previousIndex = viewControllerIndex - 1
-        
-        guard previousIndex >= 0 else {
-            return nil
-            //return viewControllerList.last
-        }
-        
-        guard viewControllerList.count > previousIndex else {
-            return nil
-        }
-        
-        return viewControllerList[previousIndex]
-    }
-    
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
-        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
-            return nil
-        }
-        
-        let nextIndex = viewControllerIndex + 1
-        
-        guard nextIndex < viewControllerList.count else {
-            return nil
-            //return viewControllerList.first
-        }
-        
-        guard viewControllerList.count > nextIndex else {
-            return nil
-        }
-        
-        return viewControllerList[nextIndex]
-    }
-    
-    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return viewControllerList.count
-    }
-    
-    
-    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first,
-            let firstViewControllerIndex = viewControllerList.firstIndex(of: firstViewController) else {
-                return 0
-        }
-        
-        return firstViewControllerIndex
-    }
-}
+//
+//import UIKit
+//
+//class PageViewControllerTest: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+//    
+//    lazy var viewControllerList: [UIViewController] = {
+//        let sb = UIStoryboard(name: "Questions", bundle: nil)
+//        let vc1 = sb.instantiateViewController(withIdentifier: "Questions1")
+//        let vc2 = sb.instantiateViewController(withIdentifier: "Questions2")
+//        //let vc3 = sb.instantiateViewController(withIdentifier: "Questions2")
+//        
+//        return [vc1, vc2]
+//    }()
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.dataSource = self
+//        self.delegate = self
+//        if let firstViewController = viewControllerList.first {
+//            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+//        }
+//    }
+//    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        for view in self.view.subviews {
+//            if view is UIScrollView {
+//                view.frame = UIScreen.main.bounds
+//            } else if view is UIPageControl {
+//                view.backgroundColor = UIColor.clear
+//            }
+//        }
+//    }
+//    
+//    
+//    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+//        
+//        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
+//            return nil
+//        }
+//        
+//        let previousIndex = viewControllerIndex - 1
+//        
+//        guard previousIndex >= 0 else {
+//            return nil
+//            //return viewControllerList.last
+//        }
+//        
+//        guard viewControllerList.count > previousIndex else {
+//            return nil
+//        }
+//        
+//        return viewControllerList[previousIndex]
+//    }
+//    
+//    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+//        
+//        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
+//            return nil
+//        }
+//        
+//        let nextIndex = viewControllerIndex + 1
+//        
+//        guard nextIndex < viewControllerList.count else {
+//            return nil
+//            //return viewControllerList.first
+//        }
+//        
+//        guard viewControllerList.count > nextIndex else {
+//            return nil
+//        }
+//        
+//        return viewControllerList[nextIndex]
+//    }
+//    
+//    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
+//        return viewControllerList.count
+//    }
+//    
+//    
+//    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        guard let firstViewController = viewControllers?.first,
+//            let firstViewControllerIndex = viewControllerList.firstIndex(of: firstViewController) else {
+//                return 0
+//        }
+//        
+//        return firstViewControllerIndex
+//    }
+//}
--- Alpha/QuestionsStory/SingleQuestion.swift
+++ Alpha/QuestionsStory/SingleQuestion.swift
@@ -1,22 +1,22 @@
+////
+////  SingleQuestion.swift
+////  Alpha
+////
+////  Created by iosdev on 03/12/2019.
+////  Copyright © 2019 Jari Pietikäinen. All rights reserved.
+////
 //
-//  SingleQuestion.swift
-//  Alpha
+//import Foundation
+//import UIKit
 //
-//  Created by iosdev on 03/12/2019.
-//  Copyright © 2019 Jari Pietikäinen. All rights reserved.
-//
-
-import Foundation
-import UIKit
-
-class SingleQuestion: NSObject {
-    
-    var question: String?
-    var answer1: String?
-    var answer2: String?
-    var answer3: String?
-    var answer4: String?
-    var answer5: String?
-    var answer6: String?
-    
-}
+//class SingleQuestion: NSObject {
+//    
+//    var question: String?
+//    var answer1: String?
+//    var answer2: String?
+//    var answer3: String?
+//    var answer4: String?
+//    var answer5: String?
+//    var answer6: String?
+//    
+//}
